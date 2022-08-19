9cd6dff75f49a992a1ced3e483ae5b24070b4659
kaique64 <kaiquehenrique.2005@outlook.com>
2022-08-19 14:47:35 -0300

    create function to make file upload

---
+++ frontend/src/service/fileUploadService.js
@@ -0,0 +1,17 @@
+import api from './api/axios'
+
+async function upload(formData) {
+    return api.post('/files/xls/upload', formData, {
+        headers: {
+            "Access-Control-Allow-Origin": "http://localhost:8000",
+            "Access-Control-Allow-Methods": "GET, POST, PATCH, PUT, DELETE, OPTIONS",
+            "Access-Control-Allow-Headers": "Origin, Content-Type, X-Auth-Token",
+            "Content-Type": "multipart/form-data"
+        },
+        responseType: 'blob'
+    }).then((response) => {
+        return response;
+    })
+}
+
+export { upload };

fa171abb9efbacf845201790bceed1874dc0e6ab
kaique64 <kaiquehenrique.2005@outlook.com>
2022-08-19 14:58:11 -0300

    create file component

---
+++ frontend/src/components/FileComponent.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+    props: {
+        filename: String,
+        accept: String,
+        class: String,
+        name: String,
+        id: String,
+        changeMethod: Function
+    }
+}
+</script>
+
+<template>
+    <label :for="id" :class="class">
+        <img src="/assets/upload-icon.svg" class="board-inventory-icon" alt="Welcome to PadUtil">      
+        {{ filename }}
+    </label>
+    <input 
+        type="file" 
+        :name="name" 
+        :id="id" 
+        :accept="accept"
+        @change="changeMethod"
+    >
+</template>

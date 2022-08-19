bb95a556f4b960643091f165485c79d313eab393
kaique64 <kaiquehenrique.2005@outlook.com>
2022-08-19 14:58:29 -0300

    create button component to download files

---
+++ frontend/src/components/DownloadButtonComponent.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+    props: {
+        title: String,
+        class: String,
+        clickMethod: Function
+    }
+}
+</script>
+
+<template>
+    <button :class="class" @click="clickMethod">
+        <img src="/assets/download-icon.svg" class="status-icon" alt="Upload feito com sucesso">
+        {{ title }}
+    </button>
+</template>
